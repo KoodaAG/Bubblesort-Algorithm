847e51b017e70ac989920c0c064a62326bbd97f4
natsud <kofi.offei.ko@gmail.com>
2015-11-20 17:45:52 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+
+
+int main()
+{
+
+	int values[10] = { 19,
+		15,
+		223,
+		1951,
+		51923,
+		1,
+		23,
+		9,
+		100,
+		10 };
+	int temp;
+
+	for (int k = 0; k < 10; ++k)
+	{
+		for (int i = 9; i >= 0; --i)
+		{
+			if (i != 0)
+			{
+				if (values[i] < values[i - 1])
+				{
+					temp = values[i];
+					values[i] = values[i - 1];
+					values[i - 1] = temp;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
